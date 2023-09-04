1327c60c125b14179ca20454acedc7d12e4dcb30
Lucas Verrastro <lucas.verrastro@gmail.com>
2023-09-04 16:18:49 -0300

    PreEntrega1 Verrastro

--- main.js
+++ main.js
@@ -1,46 +1,52 @@
-//let nombre = "Lucas";
-//let edad = 28;
-
-//const texto1 = "Hola soy ";
-//const texto2 = " y tengo ";
-//const texto3 = " años.";
-
-//alert(texto1 + nombre + texto2 + edad + texto3);
-
-function sumar(valor1, valor2){
+function sumar(valor1, valor2) {
     const resultado = valor1 + valor2;
     return resultado;
 }
-function restar(valor1, valor2){
+
+function restar(valor1, valor2) {
     const resultado = valor1 - valor2;
     return resultado;
 }
-function multiplicar(valor1, valor2){
+
+function multiplicar(valor1, valor2) {
     const resultado = valor1 * valor2;
     return resultado;
 }
-function dividir(valor1, valor2){
+
+function dividir(valor1, valor2) {
     const resultado = valor1 / valor2;
     return resultado;
 }
-function calculadora(){
-    const valor1 = parseInt(prompt("Ingrese el 1er valor:"));
-    const valor2 = parseInt(prompt("Ingrese el 2do valor:"));
-    const operacion = prompt("Que operacion queres hacer? (+ - * /)");
 
-    switch(operacion){
-        case "+":
-            alert("El resultado es " + sumar(valor1, valor2));
-        break;
-        case "-":
-            alert("El resultado es " + restar(valor1, valor2));
-        break;
-        case "*":
-            alert("El resultado es " + multiplicar(valor1, valor2));
-        break;
-        case "/":
-            alert("El resultado es " + dividir(valor1, valor2));
-        break;
+function calculadora() {
+    while (true) {
+        const valor1 = parseInt(prompt("Ingrese el 1er valor:"));
+        const operacion = prompt("Que operación queres hacer? (+ - * /) o 'salir' para salir:");
+        const valor2 = parseInt(prompt("Ingrese el 2do valor:"));
+        
+
+        if (operacion === 'salir') {
+            alert("Saliendo de la calculadora.");
+            break;
+        }
+
+        switch (operacion) {
+            case "+":
+                alert("El resultado es " + sumar(valor1, valor2));
+                break;
+            case "-":
+                alert("El resultado es " + restar(valor1, valor2));
+                break;
+            case "*":
+                alert("El resultado es " + multiplicar(valor1, valor2));
+                break;
+            case "/":
+                alert("El resultado es " + dividir(valor1, valor2));
+                break;
+            default:
+                alert("Operación no válida. Intente de nuevo.");
+        }
     }
 }
-console.log (calculadora());
+
+calculadora();
